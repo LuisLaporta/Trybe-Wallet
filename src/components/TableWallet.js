100555abdf80d3c576d93b6ae599266b28f8b526
Luis Laporta <lui.laporta99@gmail.com>
2022-08-27 13:57:47 -0300

    Renderização das despesas na table do componente TableWallet

--- src/components/TableWallet.js
+++ src/components/TableWallet.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import '../css/table.css';
 
 class tableWallet extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <thead>
@@ -20,9 +21,36 @@ class tableWallet extends Component {
             <th>Editar/Excluir</th>
           </tr>
         </thead>
+        <tbody>
+          {expenses.map(({
+            description, currency, method, tag, value, exchangeRates, id,
+          }) => (
+            <tr key={ id }>
+              <td>{description}</td>
+              <td>{tag}</td>
+              <td>{method}</td>
+              <td>{Number(value).toFixed(2)}</td>
+              <td>{exchangeRates[currency].name}</td>
+              <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+              <td>{(value * exchangeRates[currency].ask).toFixed(2)}</td>
+              <td>Real</td>
+              <td>
+                <button type="button">Editar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     );
   }
 }
 
-export default connect()(tableWallet);
+tableWallet.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  ...state.wallet,
+});
+
+export default connect(mapStateToProps)(tableWallet);
