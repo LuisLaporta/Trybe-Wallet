1d6e4cac742252454599028cfef302bcaefbffd3
LuisLaporta <lui.laporta99@gmail.com>
2023-08-14 15:15:49 -0300

    feat: implementa css do componente table

--- src/components/TableWallet.js
+++ src/components/TableWallet.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import '../css/table.css';
 import { deleteWallet, editWallet } from '../redux/actions';
+import formatarNumero from '../services/formatarNumero';
 
 class tableWallet extends Component {
   handleClick = ({ target }) => {
@@ -24,15 +25,15 @@ class tableWallet extends Component {
       <table>
         <thead>
           <tr>
-            <th>Descrição</th>
+            <th className="descri">Descrição</th>
             <th>Tag</th>
             <th>Método de pagamento</th>
-            <th>Valor</th>
+            <th className="valor">Valor</th>
             <th>Moeda</th>
             <th>Câmbio utilizado</th>
             <th>Valor convertido</th>
             <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
+            <th className="edit">Editar/Excluir</th>
           </tr>
         </thead>
         <tbody>
@@ -43,27 +44,35 @@ class tableWallet extends Component {
               <td>{description}</td>
               <td>{tag}</td>
               <td>{method}</td>
-              <td>{Number(value).toFixed(2)}</td>
+              <td>{formatarNumero(Number(value))}</td>
               <td>{exchangeRates[currency].name}</td>
               <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
-              <td>{(value * exchangeRates[currency].ask).toFixed(2)}</td>
+              <td>{formatarNumero((value * exchangeRates[currency].ask))}</td>
               <td>Real</td>
               <td>
                 <button
                   id={ id }
                   type="button"
                   data-testid="edit-btn"
+                  className="edit-btn"
                   onClick={ this.handleEdit }
                 >
-                  Editar
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                    <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                  </svg>
                 </button>
                 <button
                   id={ id }
                   type="button"
                   data-testid="delete-btn"
+                  className="delete-btn"
                   onClick={ this.handleClick }
                 >
-                  Deletar
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                  </svg>
                 </button>
               </td>
             </tr>
